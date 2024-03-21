6625ec06672145ba14a0a2b42c07b969fa26ea2c
AdrianL11 <adrianl11@me.com>
2024-03-21 11:33:42 -0400

    CORS Change

--- authentication/routes/login.go
+++ authentication/routes/login.go
@@ -43,6 +43,8 @@ func Login(c echo.Context) error {
 
 	sess.Options = &sessions.Options{
 		MaxAge:   3600 * 24 * 14, // 14 Days
+		Domain:   "localhost",
+		Secure:   true,
 		HttpOnly: true,
 		Path:     "/",
 	}
