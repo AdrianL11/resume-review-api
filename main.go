682703ee5022e04f449abe1a1b4480cfbe3f9fd6
AdrianL11 <adrianl11@me.com>
2024-03-21 11:01:10 -0400

    CORS Change

--- main.go
+++ main.go
@@ -37,7 +37,7 @@ func main() {
 	// CORS Setup
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowMethods: []string{http.MethodPost, http.MethodGet, http.MethodOptions, http.MethodHead, http.MethodPut},
-		AllowOrigins: []string{"*"},
+		AllowOrigins: []string{"https://" + os.Getenv("base_url"), "http://localhost"},
 		AllowHeaders: []string{
 			echo.HeaderOrigin,
 			echo.HeaderContentType,
