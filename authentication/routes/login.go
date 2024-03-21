5ad3557a72a447c4f30c9eba70200d0f8eee4719
AdrianL11 <adrianl11@me.com>
2024-03-21 11:41:00 -0400

    CORS Change

--- authentication/routes/login.go
+++ authentication/routes/login.go
@@ -43,10 +43,11 @@ func Login(c echo.Context) error {
 
 	sess.Options = &sessions.Options{
 		MaxAge:   3600 * 24 * 14, // 14 Days
-		Domain:   "localhost",
+		Domain:   "vdart.ai",
 		Secure:   true,
 		HttpOnly: true,
 		Path:     "/",
+		SameSite: http.SameSiteStrictMode,
 	}
 	sess.Values["email_address"] = loginDetails.Email
 	sess.Values["session_id"] = uuid.New().String()
